403d45e219cdbb39a5a2d639a139c126cc275dd3
James Seward <james.seward@teamaol.com>
2016-07-01 12:11:54 +0100

    Add git plugin

---
+++ local-plugins/git/init.zsh
@@ -0,0 +1,14 @@
+#
+# Provides Git aliases and functions.
+#
+# Authors:
+#   Sorin Ionescu <sorin.ionescu@gmail.com>
+#
+
+# Return if requirements are not found.
+if (( ! $+commands[git] )); then
+  return 1
+fi
+
+# Source module files.
+source "${0:h}/alias.zsh"
