a0b89760bab64a4e02bbad1c04986f568a301c9c
James Seward <james.seward@oath.com>
2018-01-21 15:37:46 -0500

    Increase history size

--- local-plugins/history/init.zsh
+++ local-plugins/history/init.zsh
@@ -11,8 +11,8 @@
 #
 
 HISTFILE="${ZDOTDIR:-$HOME}/.zhistory"       # The path to the history file.
-HISTSIZE=10000                   # The maximum number of events to save in the internal history.
-SAVEHIST=10000                   # The maximum number of events to save in the history file.
+HISTSIZE=50000                   # The maximum number of events to save in the internal history.
+SAVEHIST=50000                   # The maximum number of events to save in the history file.
 
 #
 # Options
