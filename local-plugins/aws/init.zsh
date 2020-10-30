5b18970e355875a5fc28f7b6a2efb06791b3b3db
James Seward <sewardjm@amazon.com>
2020-10-30 15:22:12 +0000

    Add sm-rdp command

--- local-plugins/aws/init.zsh
+++ local-plugins/aws/init.zsh
@@ -17,6 +17,7 @@ autoload get-images
 autoload get-images-info
 autoload get-windows-password
 autoload sm
+autoload sm-rdp
 
 [[ -z $AWS_PROFILE ]] && [[ -r $HOME/.cache/aws/profile ]] && export AWS_PROFILE=$(< $HOME/.cache/aws/profile )
 [[ -z $AWS_DEFAULT_REGION ]] && [[ -r $HOME/.cache/aws/region ]] && export AWS_DEFAULT_REGION=$(< $HOME/.cache/aws/region )
