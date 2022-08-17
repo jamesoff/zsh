efa159bf7ee42f5bd439a263dcea0ed0883a5ec6
James Seward <sewardjm@amazon.com>
2022-08-17 16:00:57 +0100

    Add venv command

--- local-plugins/utility/init.zsh
+++ local-plugins/utility/init.zsh
@@ -1,6 +1,6 @@
 fpath=( ~/.zsh/local-plugins/utility/autoload $fpath )
 
-autoload agvim try-ssh has git-commit-edit fv show-certificate t try reload-env pretty-path make-data-url
+autoload agvim try-ssh has git-commit-edit fv show-certificate t try reload-env pretty-path make-data-url venv
 zle -N git-commit-edit
 
 alias rv='agvim'
