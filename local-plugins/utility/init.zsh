6cb2cd42c8691cc3c72cf253844fdd8a5c83287f
James Seward <james@jamesoff.net>
2023-05-07 16:39:08 +0100

    Fix yt-dlp functions

--- local-plugins/utility/init.zsh
+++ local-plugins/utility/init.zsh
@@ -220,9 +220,17 @@ function string_hash() {
 function lolcow() { cowsay "$@" | lolcat -t }
 
 function yt-dlp-720 {
-	yt-dlp -f 'best[height=720]' -o "%(uploader)s - %(upload_date)s - %(title)s.%(ext)s" "$1"
+	yt-dlp \
+		-S 'height:720' \
+		--sponsorblock-mark all \
+		-o "%(uploader)s - %(upload_date)s - %(title)s.%(ext)s" \
+		"$1"
 }
 
 function yt-dlp-1080 {
-	yt-dlp -f 'best[height=1080]' -o "%(uploader)s - %(upload_date)s - %(title)s.%(ext)s" "$1"
+	yt-dlp \
+		-S 'height:1080' \
+		--sponsorblock-mark all \
+		-o "%(uploader)s - %(upload_date)s - %(title)s.%(ext)s" \
+		"$1"
 }
